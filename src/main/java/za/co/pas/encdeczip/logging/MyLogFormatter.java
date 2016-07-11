8051e3fbfcdfc834e1b343420190119f615f980e
André Labuschagné <andre@paranoidandroid.co.za>
2016-07-11 09:07:23 +0200

    Adding output for url text

--- src/main/java/za/co/pas/encdeczip/logging/MyLogFormatter.java
+++ src/main/java/za/co/pas/encdeczip/logging/MyLogFormatter.java
@@ -16,10 +16,23 @@ public class MyLogFormatter extends Formatter {
 
     @Override
     public String format(LogRecord record) {
+        
         StringBuilder sb = new StringBuilder();
         sb.append(record.getLevel()).append(":\t").append(record.getMessage()).append("\n");
         if(record.getThrown() != null) {
             sb.append("\t\t").append(record.getThrown().getMessage()).append("\n");
+        }
+        if(record.getParameters() != null) {
+            int i = 0;
+            for(Object o : record.getParameters()) {
+                String needle = "{" + i + "}";
+                int pos = sb.indexOf(needle);
+                if(pos != -1) {
+                    sb.delete(pos, pos + needle.length());
+                    sb.insert(pos, o.toString());
+                }
+                i++;
+            }
         }
         return  sb.toString();
     }
